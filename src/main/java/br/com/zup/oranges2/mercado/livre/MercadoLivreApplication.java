c7d880c8f10da678e6f546fd60cfdd47f5df796d
DayanaSouzaZup <dayana.souza@zup.com.br>
2021-03-02 15:38:04 -0300

    Criação do projeto

---
+++ src/main/java/br/com/zup/oranges2/mercado/livre/MercadoLivreApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.oranges2.mercado.livre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoLivreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoLivreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/oranges2/mercado/livre/MercadoLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.oranges2.mercado.livre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
